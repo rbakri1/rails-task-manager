eb7b100d85e43138dfc072b3c048582933ce7793
Rami Bakri <rbakri1@gmail.com>
2017-05-23 16:37:38 +0100

    completed exercise

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,8 @@ ActiveRecord::Schema.define(version: 20170523111149) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
-    t.string "desription"
-    t.boolean "completed"
+    t.string "description"
+    t.boolean "completed", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Task.create(name: "ironing", description: "do the ironing that you need to", completed: false)
+Task.create(name: "washing", description: "put the dark colours wasahing on", completed: true)
+Task.create(name: "make the bed", description: "make the bed before you go out", completed: false)
+
+# "rails db:seed" in Terminal => that's how you run the seed file and actually seed the database.
